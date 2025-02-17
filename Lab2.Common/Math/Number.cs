7bed2fee6eec27d9d8388b8d99cb4ee20d2b7b31
Sorokovskys <Sorokovskys@ukr.net>
2025-02-17 15:20:32 +0200

    feat: length of key

--- Lab2.Common/Math/Number.cs
+++ Lab2.Common/Math/Number.cs
@@ -39,4 +39,22 @@ public static class Number
 
         return true;
     }
+
+    public static int Gcd(IEnumerable<int> numbers)
+    {
+        return numbers.Aggregate(Gcd);
+    }
+
+    private static int Gcd(int a, int b)
+    {
+        if(b > a) (a, b) = (b, a);
+        while (b != 0)
+        {
+            var temp = b;
+            b = a % b;
+            a = temp;
+        }
+
+        return a;
+    }
 }
---
+++ Lab2.Visionary/Statistics/ThreeGrams.cs
@@ -0,0 +1,51 @@
+namespace Lab2.Visionary.Statistics;
+
+public static class ThreeGrams
+{
+    public static string MostPopularThreeGram(string input)
+    {
+        var threeGrams = FindInText(input);
+        return threeGrams
+            .MaxBy(x => x.Value).Key;
+    }
+
+    public static List<int> GetDestinations(string input, string threeGram)
+    {
+        var result = new List<int>();
+        var indexes = CollectIndexes(input, threeGram);
+        var firstIndex = indexes.First();
+        foreach (var index in indexes.Skip(1))
+        {
+            var dest = index - firstIndex;
+            result.Add(dest);
+        }
+        return result;
+    }
+    
+    private static List<int> CollectIndexes(string input, string threeGram)
+    {
+        var result = new List<int>();
+        for (var i = 2; i < input.Length; i++)
+        {
+            var startIndex = i - 2;
+            var endIndex = i;
+            var currentThreeGram = $"{input[startIndex]}{input[endIndex - 1]}{input[endIndex]}";
+            if(currentThreeGram == threeGram) result.Add(startIndex);
+        }
+        return result;
+    }
+    
+    private static Dictionary<string, int> FindInText(string input)
+    {
+        var result = new Dictionary<string, int>();
+        for (var i = 2; i < input.Length; i++)
+        {
+            var startIndex = i - 2;
+            var endIndex = i;
+            var threeGram = $"{input[startIndex]}{input[endIndex - 1]}{input[endIndex]}";
+            if(result.ContainsKey(threeGram)) result[threeGram]++;
+            else result.Add(threeGram, 1);
+        }
+        return result;
+    }
+}

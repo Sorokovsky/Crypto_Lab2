f6ca49fa0d8bc32ad54afe95eefed85204834ad2
Sorokovsky <Sorokovskys@ukr.net>
2025-02-19 22:18:51 +0200

    feat: start analysing

---
+++ Lab2.Common/Analysing/CypherAnalyzer.cs
@@ -0,0 +1,49 @@
+﻿using Lab2.Common.Math;
+using Lab2.Common.Statistics;
+using Lab2.Common.Tools;
+
+namespace Lab2.Common.Analysing;
+
+public static class CypherAnalyzer
+{
+    public static int GetKeyLength(string text)
+    {
+        text = PrepareText(text);
+        var popularThreeGram = ThreeGrams.GetMostPopularThreeGram(text);
+        var lengthBetweenFirstLetters = ThreeGrams.GetDestinations(text, popularThreeGram);
+        return Number.Gcd(lengthBetweenFirstLetters);
+    }
+
+    public static TextTable SplitByColumns(string text, int keyLength)
+    {
+        text = PrepareText(text);
+        List<List<char>> result = [];
+        var step = 0;
+        foreach (var letter in text)
+        {
+            if (step == keyLength) step = 0;
+
+            var list = result.ElementAtOrDefault(step);
+            if (list is null)
+            {
+                list = [];
+                result.Add(list);
+            }
+
+            list.Add(letter);
+            step++;
+        }
+
+        return new TextTable(result);
+    }
+
+    private static string PrepareText(string text)
+    {
+        var result = string.Empty;
+        foreach (var symbol in text)
+            if (char.IsLetter(symbol))
+                result += char.ToUpper(symbol);
+
+        return result;
+    }
+}
---
+++ Lab2.Common/Tools/TextTable.cs
@@ -0,0 +1,47 @@
+﻿namespace Lab2.Common.Tools;
+
+public class TextTable
+{
+    private readonly List<List<char>> _table;
+
+    public TextTable(List<List<char>> table)
+    {
+        _table = table;
+    }
+
+    public char GetBy(int row, int column)
+    {
+        var columns = _table.ElementAt(row);
+        return columns.ElementAt(column);
+    }
+
+    public override string ToString()
+    {
+        var result = string.Empty;
+        foreach (var cols in _table)
+        {
+            foreach (var cell in cols) Console.Write(cell);
+
+            Console.WriteLine();
+        }
+
+        return result;
+    }
+
+    public List<char> GetRow(int row)
+    {
+        return _table.ElementAtOrDefault(row) ?? [];
+    }
+
+    public List<char> GetCol(int col)
+    {
+        var result = new List<char>();
+        foreach (var row in _table)
+        {
+            var item = row.ElementAtOrDefault(col);
+            result.Add(item);
+        }
+
+        return result;
+    }
+}
